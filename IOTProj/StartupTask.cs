92c5f52a7d8e08b91aaa8213f9a0b9132008ab91
Keegan <dragonaccyip@yahoo.com.sg>
2020-11-17 22:01:05 +0800

    Temp sensor start

--- IOTProj/StartupTask.cs
+++ IOTProj/StartupTask.cs
@@ -5,12 +5,50 @@ using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
 
+using System.Diagnostics;
+using System.Threading.Tasks;
+using GrovePi;
+using GrovePi.Sensors;
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace IOTProj
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        Pin tempPin = Pin.AnalogPin0;
+
+        private System.Threading.Semaphore sm = new System.Threading.Semaphore(1, 1);
+
+        double temp = 23;
+
+        double sensorTemp;
+
+        private void Sleep(int MS)
+        {
+            Task.Delay(MS).Wait();
+        }
+        private int ReadTempADC(Pin pin)
+        {
+            sm.WaitOne();
+            int val = DeviceFactory.Build.GrovePi().AnalogRead(pin);
+            sm.Release();
+            return val;
+        }
+
+        private double getTemp()
+        {
+            int adcValue; double tempCalculated = 0, R;
+
+            adcValue = ReadTempADC(tempPin);
+
+            int B = 4250, R0 = 100000;
+            R = 100000 * (1023.0 - adcValue) / adcValue;
+            tempCalculated = 1 / (Math.Log(R / R0) / B + 1 / 298.15) - 273.15;
+            if (!Double.IsNaN(tempCalculated) && tempCalculated > 15 && tempCalculated < 40)
+                temp = tempCalculated;
+
+            return temp;
+        }
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
@@ -20,6 +58,13 @@ namespace IOTProj
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+
+            while (true)
+            {
+                Sleep(300);
+                sensorTemp = getTemp();
+                Debug.WriteLine("Temp in degrees Celsisus is " + sensorTemp.ToString("N2"));
+            }
         }
     }
 }
