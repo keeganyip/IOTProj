200fe58ace6f592d7334a56a73299a04e164ef46
donwong2308 <donwong2308@gmail.com>
2020-12-04 23:35:29 +0800

    first step

---
+++ WebForm_DB_Createuser/WebForm_DB_Createuser/WebForm1.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebForm_DB_Createuser.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="Label1" runat="server" Text="Please select a Chart Type"></asp:Label>
+    <br />
+
+    <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+        <asp:ListItem Value="Line" Selected="True">Line</asp:ListItem>
+        <asp:ListItem Value="Bar">Bar</asp:ListItem>
+    </asp:DropDownList>
+
+    <table>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <div id="MyLineChart">
+                    <asp:GridView ID="gvLineChart" runat="server">
+                    </asp:GridView>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td>
+                <div id="MyBarChart">
+                    <asp:GridView ID="gvBarChart" runat="server">
+                    </asp:GridView>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script>
+        $("#MyLineChart").highcharts({
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: "Monthly Temp"
+            },
+            xAxis: {
+                title: {
+                    text: "Date"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Temp"
+                }
+            },
+            series: [{
+                type: "spline",
+                name: "Monthly Temp",
+                data: <%=lineData%>,
+            }]
+        });
+    </script>
+
+    <script>
+        $("#MyBarChart").highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: "Monthly Temp"
+            },
+            xAxis: {
+                title: {
+                    text: "Temp"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Date"
+                }
+            },
+            series: [{
+                type: "bar",
+                name: "Monthly Temp",
+                data: <%=barData%>,
+            }]
+        });
+    </script>
+
+</asp:Content>
---
+++ WebForm_DB_Createuser/WebForm_DB_Createuser/WebForm1.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+
+
+namespace WebForm_DB_Createuser
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string lineData;
+        public string barData;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedIndex == 0)
+            {
+                LoadDataofLineChart();
+                gvBarChart.Visible = false;
+
+            }
+            if (DropDownList1.SelectedIndex == 1)
+            {
+                LoadDataofBarChart();
+                gvLineChart.Visible = false;
+
+            }
+
+        }
+
+        public void LoadDataofLineChart()
+        {
+            string strConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnectStr"].ConnectionString;
+
+            SqlConnection myConnect = new SqlConnection(strConnectionString);
+
+            myConnect.Open();
+
+            string strCommandText = "Select Date, Temp From Temperature Where EventType='HighLimit'";
+
+            SqlCommand comm = new SqlCommand(strCommandText, myConnect);
+            DataTable dt = new DataTable();
+            dt.Load(comm.ExecuteReader());
+            gvLineChart.DataSource = dt;
+            gvLineChart.DataBind();
+
+
+            //data format is [  [x1,y1], [x2,y2], ...]
+            lineData = "[";
+            foreach (DataRow dr in dt.Rows)
+            {
+                string month = Convert.ToString(dr["Date"]);
+                string TempMonth = month.Substring(0, 2);
+                Debug.WriteLine(TempMonth);
+                Debug.WriteLine(month);
+
+                lineData += "[" + TempMonth + "," + dr["Temp"] + "],";
+            }
+            lineData = lineData.Remove(lineData.Length - 1) + ']';
+        }
+
+        public void LoadDataofBarChart()
+        {
+            string strConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnectStr"].ConnectionString;
+
+            SqlConnection myConnect = new SqlConnection(strConnectionString);
+
+            myConnect.Open();
+
+            string strCommandText = "Select Date, Temp From Temperature Where EventType='HighLimit'";
+
+            SqlCommand comm = new SqlCommand(strCommandText, myConnect);
+            DataTable dt = new DataTable();
+            dt.Load(comm.ExecuteReader());
+
+            gvBarChart.DataSource = dt;
+            gvBarChart.DataBind();
+
+            //data format is [  [x1,y1], [x2,y2], ...]
+            barData = "[";
+            foreach (DataRow dr in dt.Rows)
+            {
+                string month = Convert.ToString(dr["Date"]);
+                string TempMonth = month.Substring(0, 2);
+                Debug.WriteLine(TempMonth);
+                Debug.WriteLine(month);
+
+                barData += "[" + dr["Temp"] + "," + TempMonth + "],";
+            }
+            barData = barData.Remove(barData.Length - 1) + ']';
+        }
+    }
+}
