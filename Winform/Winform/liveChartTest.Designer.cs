e804b221a47c5c425627ee959e8c2fdadf71e4e8
Afif <180865K@mymail.nyp.edu.sg>
2021-01-20 13:37:13 +0800

    added liveChart

---
+++ Winform/Winform/liveChartTest.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Winform
+{
+    partial class liveChartTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartTest1 = new System.Windows.Forms.DataGridView();
+            this.chartTest2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartTest3 = new System.Windows.Forms.DataGridView();
+            this.chartTest4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartTest1
+            // 
+            this.chartTest1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.chartTest1.Location = new System.Drawing.Point(48, 13);
+            this.chartTest1.Name = "chartTest1";
+            this.chartTest1.Size = new System.Drawing.Size(240, 150);
+            this.chartTest1.TabIndex = 0;
+            // 
+            // chartTest2
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartTest2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartTest2.Legends.Add(legend1);
+            this.chartTest2.Location = new System.Drawing.Point(357, 13);
+            this.chartTest2.Name = "chartTest2";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartTest2.Series.Add(series1);
+            this.chartTest2.Size = new System.Drawing.Size(346, 300);
+            this.chartTest2.TabIndex = 1;
+            this.chartTest2.Text = "chart1";
+            // 
+            // chartTest3
+            // 
+            this.chartTest3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.chartTest3.Location = new System.Drawing.Point(48, 366);
+            this.chartTest3.Name = "chartTest3";
+            this.chartTest3.Size = new System.Drawing.Size(240, 150);
+            this.chartTest3.TabIndex = 2;
+            // 
+            // chartTest4
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartTest4.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartTest4.Legends.Add(legend2);
+            this.chartTest4.Location = new System.Drawing.Point(357, 366);
+            this.chartTest4.Name = "chartTest4";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartTest4.Series.Add(series2);
+            this.chartTest4.Size = new System.Drawing.Size(346, 300);
+            this.chartTest4.TabIndex = 3;
+            this.chartTest4.Text = "chart1";
+            // 
+            // liveChartTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 623);
+            this.Controls.Add(this.chartTest4);
+            this.Controls.Add(this.chartTest3);
+            this.Controls.Add(this.chartTest2);
+            this.Controls.Add(this.chartTest1);
+            this.Name = "liveChartTest";
+            this.Text = "liveChartTest";
+            this.Load += new System.EventHandler(this.liveChartTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTest4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView chartTest1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartTest2;
+        private System.Windows.Forms.DataGridView chartTest3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartTest4;
+    }
+}
---
+++ Winform/Winform/liveChartTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Winform
+{
+    public partial class liveChartTest : Form
+    {
+        string strConnectionString = ConfigurationManager.ConnectionStrings["LoginDBConnection"].ConnectionString;
+
+        public liveChartTest()
+        {
+            InitializeComponent();
+        }
+
+        private void liveChartTest_Load(object sender, EventArgs e)
+        {
+            DisplayColumnAndCharts();
+            DisplayPiewChartAndData();
+        }
+
+        private void DisplayColumnAndCharts()
+        {
+            SqlConnection myConnect = new SqlConnection(strConnectionString);
+            String strCommandtext = "*INSERT SQL COMMAND HERE";
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(strCommandtext, myConnect);
+                SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+                //use DataTable to store gridview <-- Change chartTest1
+                DataTable dtTable = new DataTable();
+                adapter.Fill(dtTable);
+                //if there are records, then show in gridView
+                if (dtTable.Rows.Count > 0) {
+                    chartTest1.DataSource = dtTable;
+                    //use dataset to store data for charts
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    chartTest2.DataSource = ds;
+                    //Chart Title <-- Change chartTest2
+                    chartTest2.Titles.Add("ChartTest2");
+                    chartTest2.Series[0].ChartType = SeriesChartType.Column;
+                    chartTest2.Series[0].XValueMember = "ChartTest2 x-axis";
+                    chartTest2.Series[0].YValueMembers = "ChartTest2 y-axis";
+                    //to show value on top bar graph
+                    chartTest2.Series[0].IsValueShownAsLabel = true;
+                    //no grid for column
+                    chartTest2.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+                    chartTest2.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+                    //disable legends
+                    chartTest2.Legends[0].Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("There are no records in database");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.Message.ToString());
+            }
+            finally
+            {
+                myConnect.Close();
+            }
+        }
+
+        private void DisplayPiewChartAndData()
+        {
+            SqlConnection myConnect = new SqlConnection(strConnectionString);
+            String strCommandText = "INSERT SQL COMMAND HERE";
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(strCommandText, myConnect);
+                SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+                DataTable dtTable = new DataTable();
+                adapter.Fill(dtTable);
+                if (dtTable.Rows.Count > 0)
+                {
+                    chartTest3.DataSource = dtTable;
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    chartTest4.DataSource = ds;
+                    //Chart title
+                    chartTest4.Titles.Add("chartTest4");
+                    //Chart to appear in pie form (chartTable4)
+                    chartTest4.Series[0].ChartType = SeriesChartType.Pie;
+                    chartTest4.ChartAreas[0].Area3DStyle.Enable3D = true;
+                    chartTest4.Series[0].XValueMember = "chartTest4 x-axis"; //<-- Change x-avis value
+                    chartTest4.Series[0].YValueMembers = "chartTest4 y-axis"; //<-- Change y-value axis
+                    chartTest4.Series[0].IsValueShownAsLabel = true;
+                    //To show percentage
+                    chartTest4.Series[0].Label = "#PERCENT\n#VALY";
+                    //Enable legend based on data in x-axis
+                    chartTest4.Series[0].LegendText = "#VALX";
+                }
+                else
+                {
+                    MessageBox.Show("There are no records in the databse");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.Message.ToString());
+            }
+            finally
+            {
+                myConnect.Close();
+            }
+        }
+
+    }
+}
