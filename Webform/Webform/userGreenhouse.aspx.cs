3ce9e80fffed181000fede1d26abe43391966963
Afif <180865K@mymail.nyp.edu.sg>
2021-02-10 14:08:16 +0800

    userGreenhouse prog

--- Webform/Webform/userGreenhouse.aspx.cs
+++ Webform/Webform/userGreenhouse.aspx.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace Webform
 {
     public partial class userGreenhouse : System.Web.UI.Page
     {
+        string constr = ConfigurationManager.ConnectionStrings["UserdbConnectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            int userId = Convert.ToInt32(Session["id"].ToString());
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                string gidCmdStr = "SELECT GREENHOUSE_ENTRY_AMOUNT FROM USERTABLE WHERE UNIQUEUSERID = '" + userId + "'";
+                SqlCommand gidCmd = new SqlCommand(gidCmdStr, conn);
+                conn.Open();
+                string gidentries = gidCmd.ExecuteScalar().ToString();
+            }
         }
     }
 }
