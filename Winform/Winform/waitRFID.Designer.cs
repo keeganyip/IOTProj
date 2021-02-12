cffd1c33096556ee90515144dca0c50c8ff1bfe3
Keegan <dragonaccyip@yahoo.com.sg>
2021-02-12 23:28:14 +0800

    Design Updatev1.1

--- Winform/Winform/waitRFID.Designer.cs
+++ Winform/Winform/waitRFID.Designer.cs
@@ -32,13 +32,15 @@
             // 
             // waitRFID
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 44);
+            this.ClientSize = new System.Drawing.Size(178, 36);
             this.ControlBox = false;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "waitRFID";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "waitRFID";
+            this.UseWaitCursor = true;
             this.ResumeLayout(false);
 
         }
